8611399d145ee7960a6398271ad300f71bdc322f
Steven Palmer-Jones <spalmerjones@gmail.com>
2019-08-25 19:13:46 +0100

    add random port

--- consumer/src/test/groovy/com/example/pact/consumer/ContractTest.groovy
+++ consumer/src/test/groovy/com/example/pact/consumer/ContractTest.groovy
@@ -14,18 +14,18 @@ import static io.pactfoundation.consumer.dsl.LambdaDsl.newJsonBody
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.NONE,
-        properties = 'user-service.base-url:http://localhost:8080')
+        properties = 'user-service.base-url:http://localhost:${RANDOM_PORT}')
 class ContractTest extends Specification {
 
-//    public static RandomPortRule randomPort
-//
-//    def setupSpec() {
-//        randomPort = new RandomPortRule()
-//        randomPort.before()
-//    }
+    public static RandomPortRule randomPort
+
+    def setupSpec() {
+        randomPort = new RandomPortRule()
+        randomPort.before()
+    }
 
     @Rule
-    PactProviderRuleMk2 provider = new PactProviderRuleMk2("provider", null, 8080, this)
+    PactProviderRuleMk2 provider = new PactProviderRuleMk2("provider", null, randomPort.getPort(), this)
 
     @Autowired
     ApiClient client
